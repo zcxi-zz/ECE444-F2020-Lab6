c20adb0181eac15b38691e9f6099e392845dd26f
Chenxi Zhao <chenxizhao2008@gmail.com>
2020-10-30 20:22:30 -0600

    added database connection

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from project.app import app
+
 
 def test_index():
     tester = app.test_client()
@@ -6,3 +9,8 @@ def test_index():
 
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+
+def test_database():
+    init_db()
+    assert Path("flaskr.db").is_file()
